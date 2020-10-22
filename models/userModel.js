a435cf949a8b028109790d6d163e7eded07f021a
Shreyanshu Santosh <shreyanshuankit@users.noreply.github.com>
2020-10-23 00:20:41 +0530

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  photo: String,
+  googleId: String,
+  email: String,
+  status:Boolean
+});
+
+userSchema.plugin(findOrCreate);
+const User = new mongoose.model("User", userSchema);
+
+module.exports = {  User };
